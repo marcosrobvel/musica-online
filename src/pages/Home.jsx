fa3daee6bd106517383294f1cf7afced6dd9dd54
marcosrobvel <marcosraranjuez@gmail.com>
2025-08-19 17:37:45 +0200

    home-details

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -46,12 +46,33 @@ export const Home = () => {
           <div className="services-prices">
             <div className="services-prices__guitar-prices">
               <p>Clases de guitarra: $80/mes</p>
+              <p>Profesores: 
+                <ul>
+                  <li className="li-juan">Juan Pérez</li>
+                  <li className="li-maria">María López</li>
+                  <li className="li-pedro">Pedro González</li>
+                </ul>
+              </p>
             </div>
             <div className="services-prices__piano-prices">
               <p>Clases de piano: $100/mes</p>
+              <p>Profesores: 
+                <ul>
+                  <li className="li-laura">Laura Martínez</li>
+                  <li className="li-javier">Javier Fernández</li>
+                  <li className="li-carla">Carla Gómez</li>
+                </ul>
+              </p>
             </div>
             <div className="services-prices__bateria-prices">
               <p>Clases de batería: $70/mes</p>
+              <p>Profesores: 
+                <ul>
+                  <li className="li-andres">Andrés Ramírez</li>
+                  <li className="li-lucia">Lucía Torres</li>
+                  <li className="li-diego">Diego Martínez</li>
+                </ul>
+              </p>
             </div>
           </div>
 
